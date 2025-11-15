143a976ecc6e53b3ce1e7faa205589e7fbffef4e
Rima Ejel <ejelrima83@gmail.com>
2025-11-15 16:15:02 +0200

    save my changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ onSearch }) {
+  return (
+    <div className="flex justify-center mt-10">
+      <input
+        type="text"
+        placeholder="Search for books..."
+        className="w-1/2 p-3 border rounded-l-lg shadow-sm"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+      <button className="px-4 bg-blue-600 text-white rounded-r-lg">
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
