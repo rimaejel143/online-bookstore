b5c31dff2444543a38042fd174f18942227be2ee
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-21 21:25:02 +0200

    message

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -8,6 +8,22 @@ function Signup(){
         email: "",
         password: "",
     });
+     
+    const navigate = useNavigate();
 
+    function handleChange(e) {
+        setForm({ ...form, [e.target.name]:e.target.value});
+    }
+
+    async function handleSumbit(e) {
+        e.preventDefault();
+        const result = await signup(form);
+
+        if (result.message === "User registered successfully"){
+            alert("Signup successful!");
+        }else{
+            alert(result.message);
+        }
+    }
    
 }
