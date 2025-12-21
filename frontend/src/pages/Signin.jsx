c8fdaac4d13e40890b0d69f9186e19bd5e796817
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-21 21:31:23 +0200

    add div and form

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -15,7 +15,7 @@ function Signup(){
         setForm({ ...form, [e.target.name]:e.target.value});
     }
 
-    async function handleSumbit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
         const result = await signup(form);
 
@@ -25,5 +25,21 @@ function Signup(){
             alert(result.message);
         }
     }
+    return(
+        <div className="p-8 max-w-md mx-auto">
+      <h2 className="text-2xl mb-4">Sign Up</h2> 
+
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <input name="name" placeholder="Name" onChange={handleChange} className="border p-2 w-full" />
+         <input name="email" type="email" placeholder="Email" onChange={handleChange} className="border p-2 w-full" />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} className="border p-2 w-full" />
+          <button className="bg-black text-white px-4 py-2 w-full">
+          Sign Up
+        </button>
+
+      </form>
+      </div>
+    );
    
 }
+export default Signup;
