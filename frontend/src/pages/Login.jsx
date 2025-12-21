4a759146de8ae2b0d3a856b618d8385f6388c9a8
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-21 22:06:07 +0200

    form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,4 +25,22 @@ function Login(){
             alert(result.message);
         }
     }
+    return(
+        <div className="p-8 max-w-md mx-auto">
+      <h2 className="text-2xl mb-4">Login</h2>
+
+      <form  <form onSubmit={handleSubmit} className="space-y-4">
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} className="border p-2 w-full" />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} className="border p-2 w-full" />
+        <button className="bg-black text-white px-4 py-2 w-full">
+          Login
+        </button>
+
+
+      </form>
+
+        </div>
+    );
 }
+
+export default Login;
