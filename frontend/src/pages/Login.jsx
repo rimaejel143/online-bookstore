0aad3f7b042807c48bb24779ef8fa31923094f52
Rayan Tayba <ghinatayba18@gmail.com>
2025-12-22 20:44:03 +0200

    add divs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,26 +26,25 @@ function Login() {
     }
   };
   return (
-    <div className="p-8 max-w-md mx-auto">
-      <h2 className="text-2xl mb-4">Login</h2>
+    <div className="auth-page">
+    <div className="auth-card">
+       <h1 className="auth-title">Login</h1>
+        <p className="auth-subtitle">
+            Welcome back! Please login to continue.
+          </p>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          onChange={handleChange}
-          className="border p-2 w-full"
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-          className="border p-2 w-full"
-        />
-        <button className="bg-black text-white px-4 py-2 w-full">Login</button>
+    </div>
+    <div className="auth-body">
+      {error && <div className="auth-error">{error}</div>}
+      <form  className="auth-form" onSubmit={handleSubmit}>
+        <div className="auth-row">
+          <label className="auth-label">Email</label>
+
+        </div>
+
       </form>
+
+    </div>
     </div>
   );
 }
